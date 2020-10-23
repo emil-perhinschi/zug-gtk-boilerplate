8652d00304c9c856883b647546179e1b21450c3b
Emil Nicolaie Perhinschi <emilper@gmail.com>
2020-10-24 02:05:29 +0300

    GtkSourceView experiment

--- source/zug/frontend/gtk/mainWindow.d
+++ source/zug/frontend/gtk/mainWindow.d
@@ -10,12 +10,15 @@ import gtk.MenuBar;
 import gtk.MenuItem;
 import gtk.Box;
 import std.typecons;
+import gsv.SourceView;
+import gtk.ScrolledWindow;
+import gtk.TextBuffer;
 
 class AppMainWindow : MainWindow
 {
     string title = "Test main window";
     Box mainViewport;
-
+    ScrolledTextWindow editor_window;
     this() 
     {
         super(title);
@@ -26,12 +29,16 @@ class AppMainWindow : MainWindow
         this.add(mainBox);
 
         buildMenus(mainBox);
+        
 
         this.mainViewport = new Box(Orientation.VERTICAL, 10);
         bool expand = true;
         bool fill = true;
-        uint padding = 10;
+        uint padding = 0;
         mainBox.packStart(this.mainViewport, expand, fill, padding);
+
+        this.editor_window = new ScrolledTextWindow();
+	this.mainViewport.packStart(editor_window, expand, fill, padding); 
 
         Button button = new Button("Show size");
         button.addOnClicked(delegate void(Button b) { this.showSize(b); });
@@ -155,4 +162,59 @@ struct ItemData {
     void delegate (MenuItem i) action;
 }
 
+
+//source https://gtkdcoding.com/2019/09/10/0069-textview-and-textbuffer.html
+class ScrolledTextWindow : ScrolledWindow
+{
+	MySourceView mySourceView;
+	
+	this()
+	{
+		super();
+		
+		mySourceView = new MySourceView();
+		add(mySourceView);
+		
+	} // this()
+	
+} // class ScrolledTextWindow
+
+
+class MySourceView : SourceView
+{
+    import gsv.SourceBuffer;
+    import gsv.SourceLanguageManager;
+    import gsv.SourceLanguage;
+
+	SourceBuffer sourceBuffer;
+	string content = "I take exception to your code.";
+	
+	this()
+	{
+		super();
+		this.sourceBuffer = getBuffer();
+		this.sourceBuffer.setText(content);
+        this.setShowLineNumbers(true);
+        this.setInsertSpacesInsteadOfTabs(true);
+        this.setTabWidth(4);
+        this.setHighlightCurrentLine(true);
+
+        SourceLanguageManager slm = new SourceLanguageManager();
+        SourceLanguage source_language = slm.getLanguage("perl");
+
+        if ( source_language !is null )
+        {
+            import std.stdio: writefln;
+            this.sourceBuffer.setLanguage(dLang);
+            this.sourceBuffer.setHighlightSyntax(true);
+        }
+
+        //sourceView.modifyFont("Courier", 9);
+        this.setRightMarginPosition(72);
+        this.setShowRightMargin(true);
+        this.setAutoIndent(true);
+
+	} // this()
+
+} // class MyTextView
 
