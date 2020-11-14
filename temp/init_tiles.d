44e2c155733477daf9aeb5cc0f5ec0bf6053b8ba
Emil Nicolaie Perhinschi <eperhinschi@symmetryinvestments.com>
2020-11-15 00:07:09 +0200

    game map, test tiles, test sprites

---
+++ temp/init_tiles.d
@@ -0,0 +1,39 @@
+
+void init_tiles() {
+    import gdk.Pixbuf
+
+    Pixbuf[][] tiles = [
+        [ 
+            new Pixbuf("sprites/swamp_1_dark.png"),
+            new Pixbuf("sprites/swamp_1.png")
+        ],
+        [
+            new Pixbuf("sprites/swamp_2_dark.png"),
+            new Pixbuf("sprites/swamp_2.png")
+        ],
+        [
+            new Pixbuf("sprites/swamp_3_dark.png"),
+            new Pixbuf("sprites/swamp_3.png")
+        ],
+        [
+            new Pixbuf("sprites/forest_1_dark.png"),
+            new Pixbuf("sprites/forest_1.png")
+        ],
+        [
+            new Pixbuf("sprites/forest_2_dark.png"),
+            new Pixbuf("sprites/forest_2.png")
+        ],
+        [
+            new Pixbuf("sprites/forest_3_dark.png"),
+            new Pixbuf("sprites/forest_3.png")
+        ],
+        [
+            new Pixbuf("sprites/lowlands_forest_topdown_dark.png"),
+            new Pixbuf("sprites/lowlands_forest_topdown.png")
+        ],
+        [
+            new Pixbuf("sprites/lowlands_forest_topdown_tileable_dark.png"),
+            new Pixbuf("sprites/lowlands_forest_topdown_tileable.png")
+        ]
+    ];
+}
